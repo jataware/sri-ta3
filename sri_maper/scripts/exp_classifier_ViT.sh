aec9efbc545ccca4d5e5c9b5f853323baf559812
Vasily Zadorozhnyy <vasily.zadorozhnyy@sri.com>
2024-04-04 16:11:19 -0400

    Minor updates and window_size=5 experiments

---
+++ sri_maper/scripts/exp_classifier_ViT.sh
@@ -0,0 +1,8 @@
+### TRAINING
+# python sri_maper/src/train.py experiment="exp_mvt/exp_mvt_maevit_classifier_l22_uscont.yaml"
+
+#### TESTING
+# python sri_maper/src/test.py +experiment=exp_mvt_resnet_l22+h2_all_us.yaml ckpt_path="logs/cmta3-mvt-h2-cUS-classifier-ResNet/runs/2024-02-13_18-19-49/checkpoints/epoch_007.ckpt"
+
+##### PREDICTING
+# python sri_maper/src/map.py +experiment="exp_mvt/exp_mvt_maevit_classifier_l22_uscont.yaml" data.batch_size=128 ckpt_path="logs/cmta3-classifier-mvt/runs/2024-04-03_03-33-00/checkpoints/auprc_0.970.ckpt"
