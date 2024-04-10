934a2422867db97dc54cdc42e8d34b1177b9562c
Angel Daruna <angel.daruna@sri.com>
2024-04-09 23:34:29 -0400

    updated tutorials and reverted the config folders

--- sri_maper/src/utils/tif_utils.py
+++ sri_maper/src/utils/tif_utils.py
@@ -32,9 +32,6 @@ def write_tif(results, path, attributions_flag, datamodule):
         for tag, idx in datamodule.data_predict.tif_tags.items(): attrs[int(idx)] = tag
         output_rasters = output_rasters + attrs[:-1] # last tag is label - doesn't exist
 
-    left, top = tif_meta["transform"] * (0, 0)
-    right, bottom = tif_meta["transform"] * (tif_meta["width"], tif_meta["height"])
-
     tif_files = []
     for idx, tif_layer in enumerate(output_rasters):
         # forms tif ndarray
@@ -43,18 +40,8 @@ def write_tif(results, path, attributions_flag, datamodule):
         tif_data[result_pts[:,1], result_pts[:,0]] = data[:,idx].astype(float)
         
         # writes the output tif
-        tif_file = f"{path}/{tif_layer}_{str_bounds([left,bottom,right,top])}.tif"
+        tif_file = f"{path}/{tif_layer}.tif"
         with rio.open(tif_file, "w", **tif_meta) as out:
             out.write_band(1, tif_data)
         tif_files.append(tif_file)
     return tif_files
-
-
-def str_bounds(bounds):
-    # makes linux compatible str of lon/lat bounds
-    bounds_str = ""
-    bounds_str += f"L{str(bounds[0]).replace('.','p')}"
-    bounds_str += f"B{str(bounds[1]).replace('.','p')}"
-    bounds_str += f"R{str(bounds[2]).replace('.','p')}"
-    bounds_str += f"T{str(bounds[3]).replace('.','p')}"
-    return bounds_str
